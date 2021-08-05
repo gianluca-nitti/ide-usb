04f53e192d4a54b430a92f62d92cf49d7dc3ff89
Gianluca Nitti <gianluca.nitti96@gmail.com>
2021-08-05 13:30:00 +0200

    Increase bufsize to achieve same speed as old lib
    350kB/s with 8KiB bufsize. 16 or 32 KiB doesn't seem to make any
    difference

--- Core/Inc/tusb_config.h
+++ Core/Inc/tusb_config.h
@@ -4,4 +4,4 @@
 #define BOARD_DEVICE_RHPORT_NUM      0
 #define CFG_TUSB_RHPORT0_MODE        (OPT_MODE_DEVICE | OPT_MODE_FULL_SPEED)
 #define CFG_TUD_MSC                  1
-#define CFG_TUD_MSC_EP_BUFSIZE       512
+#define CFG_TUD_MSC_EP_BUFSIZE       8192
